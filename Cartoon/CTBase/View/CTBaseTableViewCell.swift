f2b5810b78b117eb4eaa6161dc21f4aec11cef23
Igor Xiang <xinagyuaguo@xxx.com>
2020-05-21 10:08:59 +0800

    [ADD] 添加基类tableviewcell

---
+++ Cartoon/CTBase/View/CTBaseTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CTBaseTableViewCell.swift
+//  Cartoon
+//
+//  Created by 向亚国 on 2020/5/17.
+//  Copyright © 2020 HiIgor. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+class CTBaseTableViewCell: UITableViewCell, Reusable {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open func setupLayout() {}
+}
