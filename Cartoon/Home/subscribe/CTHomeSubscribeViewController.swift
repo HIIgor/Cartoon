4e55c4eaef11a308864e134f59af974dd91e1ac8
Igor Xiang <xinagyuaguo@xxx.com>
2020-05-21 10:13:59 +0800

    [DELETE] 删除同名文件

--- Cartoon/Home/subscribe/CTHomeSubscribeViewController.swift
+++ Cartoon/Home/subscribe/CTHomeSubscribeViewController.swift
@@ -13,8 +13,21 @@ class CTHomeSubscribeViewController: CTBaseViewController {
     private var subscribeList = []()
     
     private lazy var collectionView: UICollectionView = {
-        <#statements#>
-        return <#value#>
+        let lt = UCollectionViewSectionBackgroundLayout()
+        lt.minimumInteritemSpacing = 5
+        lt.minimumLineSpacing = 10
+        let cw = UICollectionView(frame: CGRect.zero, collectionViewLayout: lt)
+        cw.backgroundColor = UIColor.background
+        cw.delegate = self
+        cw.dataSource = self
+        cw.alwaysBounceVertical = true
+        cw.register(cellType: CTComicCollectionViewCell.self)
+        cw.register(supplementaryViewType: CTComicCollectionHeaderView.self, ofKind: UICollectionView.elementKindSectionHeader)
+        cw.register(supplementaryViewType: CTComicCollectionFooterView.self, ofKind: UICollectionView.elementKindSectionFooter)
+        cw.uHead = URefreshHeader{ [weak self] in self?.setupLoadData() }
+        cw.uFoot = URefreshTipKissFooter(with: "使用妖气币可以购买订阅漫画\nVIP会员购买还有优惠哦~")
+        cw.uempty = UEmptyView { [weak self] in self?.setupLoadData() }
+        return cw
     }()
     
     override func viewDidLoad() {
